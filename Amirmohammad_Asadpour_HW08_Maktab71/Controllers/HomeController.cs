738f133e2751a849c4fd8b768df4a9c86505664d
amir asadpour <amirmohammad.asadpoor@gmail.com>
2022-05-04 22:36:10 +0430

    Add project files.

---
+++ Amirmohammad_Asadpour_HW08_Maktab71/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Amirmohammad_Asadpour_HW08_Maktab71.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Amirmohammad_Asadpour_HW08_Maktab71.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public static List<Person> personList = new List<Person>();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            if (personList.Count == 0)
+            {
+            personList.Add(new Person(123, "Ali"));
+            personList.Add(new Person(345, "Hasan"));
+            personList.Add(new Person(678, "Reza"));
+            personList.Add(new Person(901, "Amir"));
+            personList.Add(new Person(234, "Ahmad"));
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View(personList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View("Contact");
+        }
+
+        public IActionResult DeleteItem(int id)
+        {
+            personList.Remove(personList.Find(p => p.Id == id));
+            return View("Index", personList);
+        }
+        
+        public IActionResult EditItem(int id)
+        {
+            return View("Edit", personList.Find(p => p.Id == id));
+        }
+        
+        public IActionResult SubmitEditedItem(int id, string name)
+        {
+            Person editedPerson = personList.Find(p => p.Id == id);
+            if (editedPerson != null)
+            {
+                editedPerson.Name = name;
+            }
+            return View("Index", personList);
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Amirmohammad_Asadpour_HW08_Maktab71/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Amirmohammad_Asadpour_HW08_Maktab71.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
